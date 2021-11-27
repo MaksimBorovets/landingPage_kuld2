2448e98a4d8db33c57198e99254660bf19b14d8e
MaksimBorovets <maksimborovets1992@gmail.com>
2021-11-27 19:20:32 +0200

    fix try 1

---
+++ src/components/HeroMobile.js
@@ -0,0 +1,119 @@
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+import heroImg from "../forGalery/6.jpg";
+import styled from "styled-components";
+
+
+function HeroMobile() {
+  return (
+
+        <HeroContainer>
+          <Img src={heroImg} />
+          <h1>KUBA LANDSCAPE DESIGN</h1>
+          <p>Озеленение, автоматический полив, ландшафтный дизайн </p>
+          <div className="hero-btns">
+            <a
+              href="https://www.instagram.com/poliv.ozelenenie/"
+              target="_blank"
+            >
+              <Button
+                className="btns"
+                buttonStyle="btn--outline"
+                buttonSize="btn--large"
+              >
+                @poliv.ozelenenie
+                <i style={{ marginLeft: 5 }} class="fab fa-instagram" />
+              </Button>
+            </a>
+            <a href="tel:+380963644865">
+              <Button
+                toPage="galery"
+                className="btns"
+                buttonStyle="btn--primary"
+                buttonSize="btn--large"
+              >
+                +380963644865 <i class="fas fa-phone"></i>
+              </Button>
+            </a>
+          </div>
+        </HeroContainer>
+
+  );
+}
+
+export default HeroMobile;
+
+
+const Img = styled.img`
+  pointer-events: none;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+`;
+const HeroContainer = styled.div`
+  /* background: url('/images/img-home.jpg') center center/cover no-repeat; */
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: inset 0 0 0 1000px rgba(0, 0, 0, 0.2);
+  object-fit: contain;
+  h1 {
+    color: #fff;
+    font-size: 100px;
+    margin-top: -100px;
+  }
+  .hero-btns {
+  margin-top: 32px;
+}
+
+
+.hero-btns .btn {
+  margin: 6px;
+}
+
+.fa-play-circle {
+  margin-left: 4px;
+}
+
+
+  p {
+    margin-top: 8px;
+    color: #fff;
+    font-size: 32px;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+  }
+
+  @media screen and (max-width: 960px) {
+  h1 {
+    font-size: 70px;
+    margin-top: -150px;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  h1 {
+    font-size: 50px;
+    margin-top: -100px;
+  }
+
+  .hero-container > p {
+    font-size: 30px;
+  }
+
+  .btn-mobile {
+    display: block;
+    text-decoration: none;
+  }
+
+  .btn {
+    width: 100%;
+  }
+}
+`;
